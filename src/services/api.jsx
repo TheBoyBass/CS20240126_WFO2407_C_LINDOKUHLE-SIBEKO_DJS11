7345f731dcb005ce1bbeb0dc6570f1333421eea3
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-27 05:12:50 +0200

    "Added error handling to fetchPreviews function"

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,4 +1,7 @@
 export const fetchPreviews = async () => {
     const response = await fetch('https://podcast-api.netlify.app');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: Failed to fetch previews`);
+    }
     return response.json();
 };
