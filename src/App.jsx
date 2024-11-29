8df2986cb1d9947e284e34382e95c750010b4aff
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-29 04:27:49 +0200

    "Remove ShowDetail and EpisodeList components, and onSelectShow prop from ShowList"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react'
 import Header from './components/Header';
 import ShowList from './components/ShowList';
-import ShowDetail from './components/ShowDetail';
 import './styles/App.css'
 
 function App() {
-  const [setSelectedShow, selectedShow] = useState(null);
 
   return (
     <div className="app">
       <Header />
-      <ShowList onSelectShow={setSelectedShow} />
-      <ShowDetail show={selectedShow} />
+      <ShowList/>
     </div>
   );
 };
